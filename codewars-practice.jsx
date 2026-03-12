d875cf0610757a13abdc569cdaae8d92887a24da
augustkeller <guskeller@gmail.com>
2026-03-12 12:11:04 -0500

    exercise 2 prepped more

--- codewars-practice.jsx
+++ codewars-practice.jsx
@@ -19,3 +19,8 @@ function squareSum(numbers){
 
 //Exercise 2
 //Given an array of integers, return a new array with each value doubled.
+//For example: [1, 2, 3] --> [2, 4, 6]
+
+function maps(x){
+
+}
