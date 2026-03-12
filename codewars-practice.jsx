095c14b1365c790217ad16dd68544d3cac44dc02
augustkeller <guskeller@gmail.com>
2026-03-12 12:04:21 -0500

    for of loop and initial array added

--- codewars-practice.jsx
+++ codewars-practice.jsx
@@ -2,5 +2,9 @@
 //Complete the square sum function so that it squares each number passed into it and then sums the results together.
 
 function squareSum(numbers){
-
+    let squares = [];
+    for (const number of numbers) {
+        squares.push(number*number)
+    }
+    
 }
