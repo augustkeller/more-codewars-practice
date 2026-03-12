2f2cd5d2b32ef228732c40f2cdd10ed65d5bbb74
augustkeller <guskeller@gmail.com>
2026-03-12 12:14:27 -0500

    top answer of exercise 2 added

--- codewars-practice.jsx
+++ codewars-practice.jsx
@@ -1,6 +1,5 @@
 //Exercise 1
 //Complete the square sum function so that it squares each number passed into it and then sums the results together.
-
 //My Answer
 function squareSum(numbers){
     let squares = [];
@@ -9,7 +8,6 @@ function squareSum(numbers){
     }
     return squares.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
 }
-
 //Top Answer
 function squareSum(numbers){
   return numbers.reduce(function(sum, n){
@@ -20,11 +18,15 @@ function squareSum(numbers){
 //Exercise 2
 //Given an array of integers, return a new array with each value doubled.
 //For example: [1, 2, 3] --> [2, 4, 6]
-
+//My Answer
 function maps(x){
     let doubles = [];
     for (const num of x) {
         doubles.push(num*2)
     }
     return doubles;
+}
+//Top Answer
+function maps(x){
+  return x.map(n => n * 2);
 }
