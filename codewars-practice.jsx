ceb956dc94df6ce364989581f2b5101a1591a6fc
augustkeller <guskeller@gmail.com>
2026-03-13 12:17:07 -0500

    exercise 6 prepped

--- codewars-practice.jsx
+++ codewars-practice.jsx
@@ -76,3 +76,17 @@ function smash (words) {
 smash = function (words) {
   return words.join(" ");
 };
+
+//Exercise 6
+//Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively. Each function returns one number.
+//Example: [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+//My Answer
+var min = function(list){
+    
+    return list[0];
+}
+
+var max = function(list){
+    
+    return list[0];
+}
