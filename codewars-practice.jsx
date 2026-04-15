13aa6e1cad933eeabd46fe3c9b12714cbe13072e
augustkeller <guskeller@gmail.com>
2026-04-15 12:17:38 -0500

    exercise 9 prepped

--- codewars-practice.jsx
+++ codewars-practice.jsx
@@ -103,12 +103,22 @@
 //   return str[str.length - 1] + solution(str.slice(0, -1));
 // }
 
-//Exercise 8
-//We need a function that can transform a number (integer) into a string.
+// //Exercise 8
+// //We need a function that can transform a number (integer) into a string.
+// //Example:
+// //123  --> "123"
+// // 999  --> "999"
+// // -100 --> "-100"
+// function numberToString(num) {
+//   return num.toString();
+// }
+
+//Exercise 9
+//Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with the lowest index. If you get an empty array/list, return an empty array/list.
 //Example:
-//123  --> "123"
-// 999  --> "999"
-// -100 --> "-100"
-function numberToString(num) {
-  return num.toString();
+// * Input: [1,2,3,4,5], output = [2,3,4,5]
+// * Input: [5,3,2,1,4], output = [5,3,2,4]
+// * Input: [2,2,1,2,1], output = [2,2,2,1]
+function removeSmallest(numbers) {
+  return [];
 }
