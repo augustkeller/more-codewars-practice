e9791b8ddc396ac7ccfa75e9b487d2559aa01a8b
augustkeller <guskeller@gmail.com>
2026-04-16 12:15:15 -0500

    Exercise 10 first attempt

--- codewars-practice.jsx
+++ codewars-practice.jsx
@@ -135,11 +135,13 @@
 //Exercise 10
 //Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
 function fakeBin(x){
+  let answer = [];
   for (let i = 0; i < x.length; i++) {
     if (i < 5) {
-      
+      answer.push(0);
     } else {
-      
+      answer.push(1);
     }
   }
+  return answer;
 }
