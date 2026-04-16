901c41c2e3eaa50fd4e7027c59df170400897efd
augustkeller <guskeller@gmail.com>
2026-04-16 12:12:20 -0500

    if else statement nested in for loop of exercise 10

--- codewars-practice.jsx
+++ codewars-practice.jsx
@@ -136,6 +136,10 @@
 //Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
 function fakeBin(x){
   for (let i = 0; i < x.length; i++) {
-    
+    if (i < 5) {
+      
+    } else {
+      
+    }
   }
 }
